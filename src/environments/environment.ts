0c9d3b46ee83bfc5c6318b9b67b1cb066ca115f3
IsaquiasMarques <patisaquias2000@gmail.com>
2024-12-13 17:46:10 +0100

    google analytics integration 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     apiUrl: 'https://l8gpwvk6exqhhidouqjnvv4wec1rs1hn.nerdculture.ao',
     apiGetInTouchUrl: 'https://mailme.nerdculture.ao',
-    googleAnalyticsId: 'G-TEWCDVCV4F'
+    googleAnalyticsId: 'G-3VZSPJD4RH'
 };
